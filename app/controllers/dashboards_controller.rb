c518c8f8e47180f45e0a8a1f8459e08e07dfb145
Varrahan <varrahan@gmail.com>
2026-03-03 17:45:48 -0500

    Update to support cookies

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -4,4 +4,14 @@ class DashboardsController < ApplicationController
     @critical_count   = Anomaly.critical.count
     @total_count      = Anomaly.count
   end
+
+  private
+
+  def set_cable_cookie
+    cookies.signed[:cable_token] = {
+      value:    ENV.fetch("CABLE_TOKEN", "anonymous"),
+      httponly: true,
+      same_site: :strict
+    }
+  end
 end
